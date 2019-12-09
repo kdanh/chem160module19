334e58c7597b91c920cbf32b2b3c21ef0dbc8fee
Kyle Danh <49820747+kdanh@users.noreply.github.com>
2019-12-08 22:39:31 -0800

    Add files via upload

---
+++ count_aa.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $# -ne 2 ]; then
+   echo "Script takes 2 arguments, an AA and a pdb file"
+   exit 1
+fi
+if [ ! -f $2 ]; then
+     echo "There is no file $2"
+     exit 1
+fi
+echo -n "The number of $1 amino acids in $2 is:"
+grep $1 $2 | grep CA | wc | colrm 10
+for i in GLU GLN GLY ALA PHE
+do
+count_aa.bash  $i  dynein.pdb
+done
+chmod +x count_aa.bash
+
